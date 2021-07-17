eda414c4b0e62f78b52a87b0bde5d1cb0bc9ea5a
Akiho Ozawa <akiho@AkihonoMac-mini.local>
2021-07-18 00:39:48 +0900

    first commit

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Akiho Ozawa on 2021/07/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
